a2ca83954f80318637147b58ff449efe44b2b209
TEA <now4ever@naver.com>
2020-07-15 17:31:11 +0900

    by master home contr master 내용 추가

--- sp_test/src/main/java/com/will/sptest/HomeController.java
+++ sp_test/src/main/java/com/will/sptest/HomeController.java
@@ -22,7 +22,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("[[ Welcome home ]] The client locale is {}.", locale);
+		logger.info("[[ Welcome home - by master ]] The client locale is {}.", locale);
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
